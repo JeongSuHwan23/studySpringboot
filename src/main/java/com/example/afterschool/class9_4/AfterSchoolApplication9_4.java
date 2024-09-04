f4c69c99738d5d7f6c25e4f7185eccad0555b114
jeongsuhwan <short9666@gmail.com>
2024-09-04 19:07:16 +0900

    9-4 SpringBoot afterclass

--- src/main/java/com/example/afterschool/class9_4/AfterSchoolApplication9_4.java
+++ src/main/java/com/example/afterschool/class9_4/AfterSchoolApplication9_4.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AfterSchoolApplication9_4 {
-
   public static void main(String[] args) {
     SpringApplication.run(AfterSchoolApplication9_4.class, args);
     System.out.println("연결 완료");
